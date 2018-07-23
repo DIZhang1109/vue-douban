9153d70dffb3a4e75afac3c8bcbdafd0642e6af8
Di Zhang <170518Yaqi>
2018-07-23 23:42:22 +1200

    Add load-awesome as loading animation

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/sass/styles.scss'
+import '../node_modules/load-awesome/css/square-jelly-box.min.css'
 
 Vue.config.productionTip = false
 
