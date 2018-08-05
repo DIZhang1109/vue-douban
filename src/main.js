33e1bd1e95e09aaa25b9831b3a84d089ac171028
Di Zhang <170518Yaqi>
2018-08-05 17:34:49 +1200

    register vuex store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store/index'
 import './assets/sass/styles.scss'
 import '../node_modules/load-awesome/css/square-jelly-box.min.css'
 
@@ -8,5 +9,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
